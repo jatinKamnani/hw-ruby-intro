973f039d333595bb7b1d095d5b5640281b0d43dc
EC2 Default User <ec2-user@ip-172-31-4-121.us-east-2.compute.internal>
2019-01-30 03:16:07 +0000

    ruby assignment 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,62 @@
 
 # Part 1
 
+# def sum arr
+#   # YOUR CODE HERE
+# end
 def sum arr
   # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum elements
+  sum(elements.sort.last(2))
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
 
+def sum_to_n? arr, tot
+  if arr.empty?
+    if tot == 0 
+      return false 
+    end
+  else
+    arr.combination(2).to_a.each do |pair|
+      return true if sum(pair) == tot
+    end
+  end
+
+  return false
+end
 # Part 2
 
 def hello(name)
+    "Hello, " + name
   # YOUR CODE HERE
 end
 
 def starts_with_consonant? s
+  (s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
   # YOUR CODE HERE
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /(^0|^[10]*00)$/    
 end
-
 # Part 3
 
+
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
 end
